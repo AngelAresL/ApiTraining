57f9a2db759dc9af7f48fb3443b9df40d39dd942
Naima El Gargari Sampedro <naimasamp@gmail.com>
2023-12-08 13:43:46 +0100

    implantación código modify

--- src/controllers/training/modifyTraining.js
+++ src/controllers/training/modifyTraining.js
@@ -10,4 +10,27 @@ const modifyTraining = async (req, res, next) => {
   }
 };
 
+modifyTraining: (req, res) => {
+  const trainingId = req.params.trainingId;
+  const { name, description, exercises, date } = req.body;
+
+  db.query(
+    "UPDATE trainings SET name = ?, description = ?, exercises = ?, date = ? WHERE id = ?",
+    [name, description, JSON.stringify(exercises), date, trainingId],
+    (error, result) => {
+      if (error) {
+        res.status(500).json({ error: "Error al actualizar el entrenamiento" });
+        return;
+      }
+
+      if (result.affectedRows === 0) {
+        res.status(404).json({ message: "Entrenamiento no encontrado" });
+        return;
+      }
+
+      res.status(200).json({ message: "Entrenamiento actualizado correctamente" });
+    }
+  );
+}
+
 export default modifyTraining;
