9043909f9db9b97a9c0c0c5a2a83a307b7dc6c0e
David Barreira <barreirasuarezdavid.gmail.com>
2023-12-08 15:02:01 +0100

    Primer commit de login

--- src/controllers/users/login.js
+++ src/controllers/users/login.js
@@ -1,10 +1,34 @@
-import generateError from '../../helpers/generateError.js';
+
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { selectUserByEmail } from "../../models/users/index.js";
+import { generateError } from "../../utils/index.js";
+import { TOKEN_SECRET } from "../../../env.js";
+
 const login = async (req, res, next) => {
   try {
-    res.send({
-      status: 'error',
-      message: 'no implementada',
+    const { email, password } = req.body;
+
+    const userDb = await selectUserByEmail(email);
+
+    if (!userDb) {
+      generateError("Los datos no son correctos", 400);
+    }
+
+    const isPasswordOk = await bcrypt.compare(password, userDb.password);
+
+    if (!isPasswordOk) {
+      generateError("Los datos no son correctos", 400);
+    }
+
+    const jwtPayload = { id: userDb.id };
+
+    const token = jwt.sign(jwtPayload, TOKEN_SECRET, {
+      expiresIn: "30d",
     });
+
+    res.send({ message: "Loggeado correctamente", data: { token } });
+    
   } catch (error) {
     next(error);
   }
