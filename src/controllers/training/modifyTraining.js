28d90a6accd82bdf59b48c7c1065fefb3fe6d64b
Naima El Gargari Sampedro <naimasamp@gmail.com>
2023-12-08 20:45:51 +0100

    cambios en modify recomendados

--- src/controllers/training/modifyTraining.js
+++ src/controllers/training/modifyTraining.js
@@ -1,36 +1,49 @@
 import generateError from '../../helpers/generateError.js';
+import pool from '../../db/pool.js';
+
+
 const modifyTraining = async (req, res, next) => {
   try {
-    res.send({
-      status: 'error',
-      message: 'no implementada',
-    });
+    const trainingId = req.params.id;
+    const { name, description, photo, typology: typology, muscle_group, id_user } = req.body;
+
+    //Comprobamos si el id de entrenamiento existe
+    const rowExists =  await pool.query("SELECT * FROM training WHERE id = ?;", [trainingId]);
+
+    if (!rowExists) {
+      generateError("El entrenamiento no existe", 404); 
+    }
+
+    //Comprobamos si el usuario es admin o normal
+    const rolUser = req.auth.rol;
+  
+    if (rolUser === "normal") {
+      generateError("Debes de ser administrador para modificar entrenamientos", 403);
+    }
+
+    console.log(name, description, photo, typology, muscle_group, id_user, trainingId);
+
+    //Actualizamos los datos 
+    const [{changes}]= await pool.query(`
+    UPDATE training SET name=?, description=?, photo=?, typology=?, muscle_group=?, id_user=? WHERE id=?`,
+    [name, description, photo, typology, muscle_group, id_user, trainingId]
+    );
+
+    if (changes === 0) {
+      generateError("Debes cambiar algún dato del entrenamiento", 400);
+    }
+    
+    //const updateTraining= await selectTrainingById(trainingId);
+    const updatedTraining = await pool.query("SELECT * FROM training WHERE id = ?;", [trainingId]);
+
+    res.status(200).json({ 
+      message: "Entrenamiento actualizado correctamente",
+      data: updatedTraining
+   });
+
   } catch (error) {
     next(error);
   }
 };
-
-modifyTraining: (req, res) => {
-  const trainingId = req.params.trainingId;
-  const { name, description, exercises, date } = req.body;
-
-  db.query(
-    "UPDATE trainings SET name = ?, description = ?, exercises = ?, date = ? WHERE id = ?",
-    [name, description, JSON.stringify(exercises), date, trainingId],
-    (error, result) => {
-      if (error) {
-        res.status(500).json({ error: "Error al actualizar el entrenamiento" });
-        return;
-      }
-
-      if (result.affectedRows === 0) {
-        res.status(404).json({ message: "Entrenamiento no encontrado" });
-        return;
-      }
-
-      res.status(200).json({ message: "Entrenamiento actualizado correctamente" });
-    }
-  );
-}
 
 export default modifyTraining;
