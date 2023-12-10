815d75bcaa33916475fff31a1368394829b45033
David <142534572+Barreira83@users.noreply.github.com>
2023-12-10 16:49:59 +0100

    Resolviendo repeticion de likes

--- src/controllers/training/addLike.js
+++ src/controllers/training/addLike.js
@@ -11,17 +11,30 @@ try{
     //Cogemos el id del payload de usuario logeado
     const loggedUserId = req.auth.id;
     
+//---------------------------------------------------------------------------------------------
+    //Necesito saber cuantos registros hay, y despues lanzar error si se cumple el if
+    for (let i=1; i<15; i++){
+        let [row] = await selectLikeById(i);
+        console.log(row.id_user,loggedUserId, row.id_training,trainingId);
+
+        if( row.id_user===loggedUserId && row.id_training===trainingId){
+            generateError('Este entrenamiento ya tiene like de este usuario', 400);
+        }
+
+    }
+//-------------------------------------------------------------------------------------------
+
 
     //Insertamos id de usuario logeado e id de training en la tabla likes        
     const likeId = await insertLike(trainingId, loggedUserId);
-    console.log(likeId);
+    //console.log(likeId);
 
     if(!likeId){
         generateError('Ha ocurrido un error dando like', 400);
     }
     //Comprobamos que se añadió el like a la tabla
     const result = await selectLikeById(likeId);
-    console.log(result);
+    //console.log(result);
     if(!result){
         generateError('Ha ocurrido un error consultando el likeId', 400);
     }
