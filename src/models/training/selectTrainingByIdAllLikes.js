3006036b086577ee4f3277ab3558880aa762ad8e
David <142534572+Barreira83@users.noreply.github.com>
2024-01-15 21:45:06 +0100

    selectTrainingByIdAllLikes con BIT_OR muestra si usuario logeado dio like o favorito a entreno

--- src/models/training/selectTrainingByIdAllLikes.js
+++ src/models/training/selectTrainingByIdAllLikes.js
@@ -1,13 +1,15 @@
 import pool from '../../db/pool.js';
 // Función para seleccionar entrenamiento por Id.
-const selectTrainingByIdAllLikes = async (id) => {
+const selectTrainingByIdAllLikes = async (loggedId, id) => {
   const [[training]] = await pool.query(
-    `SELECT count(l.id_training) AS allLikes, t.name, t.description, t.photo, t.typology, t.muscle_group, t.created_at 
+    `SELECT count(l.id_training) AS allLikes, BIT_OR(l.id_user=?) LikeTrue,  BIT_OR(f.id_user=?) FavTrue,  t.name, t.description, t.photo, t.typology, t.muscle_group, t.created_at 
     FROM training t
     LEFT JOIN likes l ON l.id_training = t.id	
+    LEFT JOIN favorites f ON f.id_training = t.id
+
     GROUP BY (t.id)
     HAVING t.id=?;`,
-    [id]
+    [loggedId, loggedId, id]
   );
 
   return training;
