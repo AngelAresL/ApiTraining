b8f2c8c94464acb1a73a3efc705b8b7756fb8dc4
Angel Ares Lago <a.areslago@gmail.com>
2024-01-19 18:48:10 +0100

    cambio en ruta de link de correo forgotPassword y pool

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,11 +1,12 @@
 import mysql from 'mysql2/promise';
-import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD } from "../../env.js";;
+import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD } from '../../env.js';
 // Creamos conexión con la base de datos
 const pool = mysql.createPool({
   host: DB_HOST,
   port: DB_PORT,
   user: DB_USER,
   password: DB_PASSWORD,
+  connectionLimit: 20,
 });
 
 export default pool;
